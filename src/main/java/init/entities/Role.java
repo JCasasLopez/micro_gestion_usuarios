78b94fb87b2f7e890608aebf464602644d5ea856
Jorge Casas <j.casas.lopez.26@gmail.com>
2024-12-16 12:12:13 +0100

    Entidades

---
+++ src/main/java/init/entities/Role.java
@@ -0,0 +1,58 @@
+package init.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="roles")
+public class Role {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idRole;
+	@Column(unique=true)
+	private String nombre;
+	@ManyToMany()
+	private Set<Usuario> usuarios = new HashSet<>();
+	
+	public Role(int idRole, String nombre) {
+		this.idRole = idRole;
+		this.nombre = nombre;
+	}
+
+	public Role() {
+		super();
+	}
+
+	public int getIdRole() {
+		return idRole;
+	}
+
+	public void setIdRole(int idRole) {
+		this.idRole = idRole;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Set<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(Set<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+	
+}
---
+++ src/main/java/init/entities/Usuario.java
@@ -0,0 +1,84 @@
+package init.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idUsuario;
+	@Column(unique=true)
+	private String email;
+	@Column(unique=true)
+	private String username;
+	private String password;
+	@ManyToMany()
+	@JoinTable(name="user_roles",
+	joinColumns=@JoinColumn(name="idUsuario", referencedColumnName="idUsuario"),
+	inverseJoinColumns=@JoinColumn(name="idRole", referencedColumnName="idRole"))
+	private Set<Role> roles = new HashSet<>();
+	
+	public Usuario(int idUsuario, String email, String username, String password) {
+		this.idUsuario = idUsuario;
+		this.email = email;
+		this.username = username;
+		this.password = password;
+	}
+
+	public Usuario() {
+		super();
+	}
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+	
+}
