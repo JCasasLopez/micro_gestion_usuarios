e1ffd52ffb88703d1e336f6eb64824f732a8b584
Jorge Casas <j.casas.lopez.26@gmail.com>
2024-12-17 18:04:59 +0100

    wip: UsuariosServiceTests

--- src/test/java/init/UsuariosServiceTests.java
+++ src/test/java/init/UsuariosServiceTests.java
@@ -2,7 +2,10 @@ package init;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import init.dao.UsuariosDao;
 import init.entities.Usuario;
+import init.exception.DuplicateEmailException;
 import init.model.UsuarioDto;
 import init.service.UsuariosServiceImpl;
 import init.utilidades.Mapeador;
@@ -35,12 +39,11 @@ public class UsuariosServiceTests {
 		//Arrange
 		UsuarioDto usuarioDto = new UsuarioDto("a@gmail.com", "Yorch", "1234");
 		Usuario usuario = new Usuario(0, "a@gmail.com", "Yorch", "1234");
-		
+		when(usuariosDao.findByEmail(usuario.getEmail())).thenReturn(null);
+		when(usuariosDao.findByUsername(usuario.getUsername())).thenReturn(null);
 		when(mapeador.usuarioDtoNuevoUsuarioToEntity(usuarioDto)).thenReturn(usuario);
 		when(usuariosDao.save(usuario)).thenReturn(usuario);
 		when(mapeador.usuarioEntityToDto(usuario)).thenReturn(usuarioDto);
-		when(usuariosDao.findByEmail(usuario.getEmail())).thenReturn(null);
-		when(usuariosDao.findByUsername(usuario.getUsername())).thenReturn(null);
 		
 		//Act
 		UsuarioDto usuarioPersistido = usuariosServiceImpl.altaUsuario(usuarioDto);
@@ -53,6 +56,22 @@ public class UsuariosServiceTests {
 																			"Los usernames no coinciden"),
 				() -> assertEquals(usuario.getPassword(), usuarioPersistido.getPassword(),
 																			"Las contraseñas no coinciden"));
+	}
+	
+	@Test
+	@DisplayName("El usuario NO se puede dar de alta - Emails duplicados")
+	public void altaUsuario_emailsDuplicados() {
+		//Arrange
+		UsuarioDto usuarioDto = new UsuarioDto("a@gmail.com", "Yorch", "1234");
+		Usuario usuario = new Usuario(0, "a@gmail.com", "Yorch", "1234");
+		List<Usuario> usuarios = List.of();
+		when(usuariosDao.findByEmail(usuario.getEmail())).thenReturn(usuarios);
+		
+		//Act & Assert
+		assertThrows(DuplicateEmailException.class, () -> {
+			usuariosServiceImpl.altaUsuario(usuarioDto);
+		}, "Debería haberse lanzado una excepción DuplicateEmailException, pero no se ha lanzado");
+		
 	}
 
 }
