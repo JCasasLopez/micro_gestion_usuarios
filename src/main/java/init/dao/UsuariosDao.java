30808d3e692f6d4c5d0dccb97b92ac10acb8a4f3
Jorge Casas <j.casas.lopez.26@gmail.com>
2024-12-17 12:03:52 +0100

    UsuariosDao: añadidos findByEmail() y findByUsername()

--- src/main/java/init/dao/UsuariosDao.java
+++ src/main/java/init/dao/UsuariosDao.java
@@ -1,9 +1,12 @@
 package init.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import init.entities.Usuario;
 
 public interface UsuariosDao extends JpaRepository<Usuario, Integer> {
-
+	List<Usuario> findByEmail(String email);
+	List<Usuario> findByUsername(String username);
 }
